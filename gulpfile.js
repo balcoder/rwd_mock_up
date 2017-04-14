10e6e6ca99cd092515b07dc6a9a01b6e3ae5d83a
Des Barrett <des.barrett@gmail.com>
2017-04-14 20:39:24 +0100

    Added workflow task runners using npm gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var browsersync = require('browser-sync').create();
+
+gulp.task('css', function(){
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('dist/css'))
+});
